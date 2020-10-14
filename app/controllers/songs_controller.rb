e9459fbb3b798da94da4e39d62d3cd67c301d755
Michael Lynch <michael.lynch8211@gmail.com>
2020-10-14 16:04:46 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @songs = Artist.find(params[:artist_id]).songs
+      @artist = Artist.find(params[:artist_id]).songs
       if @artist.nil?
         redirect_to artists_path, alert: "Artist not found"
       else
